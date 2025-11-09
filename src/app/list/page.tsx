ed14016edcc45816fa873ac3a73a4bc9b1363d67
Ian Ohari <notepiccake@gmail.com>
2025-11-08 21:31:31 -1000

    notes-1 finished in 47 minutes.

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -29,6 +29,10 @@ const ListPage = async () => {
     where: { owner },
   });
 
+  const notes = await prisma.note.findMany({
+    where: { owner },
+  });
+
   return (
     <main>
       <Container id="list" fluid className="py-3">
@@ -40,7 +44,7 @@ const ListPage = async () => {
               <Row xs={1} md={2} lg={3} className="g-4">
                 {contacts.map((contact) => (
                   <Col key={`${contact.firstName}${contact.lastName}`}>
-                    <ContactCard contact={contact} />
+                    <ContactCard contact={contact} notes={notes.filter(note => note.contactId === contact.id)} />
                   </Col>
                 ))}
               </Row>
---
+++ src/components/NoteItem.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { Note } from '@prisma/client';
+import { ListGroup } from 'react-bootstrap';
+// import { Link } from 'react-bootstrap-icons';
+
+/* Renders a single row in the List Stuff table. See list/page.tsx. */
+const NoteItem = ({ note }: { note: Note }) => (
+  <ListGroup.Item>
+    <p className="fw-lighter">{note.createdAt.toLocaleDateString('en-US')}</p>
+    <p>{note.note}</p>
+  </ListGroup.Item>
+);
+
+export default NoteItem;
